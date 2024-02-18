f8b5604b2fe9cc7b4fded694252143d72a50a108
Jacob Schoerner <jacob.schoerner@gmail.com>
2024-02-18 16:16:07 +0100

    stuff

--- notify.sh
+++ notify.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+cd /home/pi/code/bostad
+echo "RUNNING SCRAPE"
+echo $(date)
 ./scrape.sh
+echo "SCRAPE DONE"
 source .venv/bin/activate
+echo "RUNNING PARSE/NOTIFY"
 python notify.py
+echo "NOTIFY DONE"
+echo "================"
