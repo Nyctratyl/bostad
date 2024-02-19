1a4fc3a0a7628a09f7ca310ea16db33f15397bb6
Jacob Schoerner <jacob.schoerner@modularfinance.se>
2024-02-19 18:17:39 +0100

    improve scraper

--- scrape.sh
+++ scrape.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+BUILDID=$(curl -s https://www.hemnet.se/bostader | grep "buildId.*isFallback" -o | cut -c -10 --complement | rev | cut -c -13 --complement | rev)
 for page in {1..49}
 do
     echo "scraping, page: ${page}"
-    curl -s "https://www.hemnet.se/_next/data/c2HT-Zoku_fJGOv-ygdmH/bostader.json?location_ids[]=18031&page=${page}" > ./data/page_nr_${page}.json
+    curl -s "https://www.hemnet.se/_next/data/${BUILDID}/bostader.json?location_ids[]=18031&page=${page}" > ./data/page_nr_${page}.json
 done
